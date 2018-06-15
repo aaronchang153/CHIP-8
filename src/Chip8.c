e22cbaf72f86b89c709e6d52323fc9141d66c778
Aaron Chang <changac2@uci.edu>
2018-06-15 13:26:27 -0700

    Added an array of function pointers and started on the implementing the emulation cycle

--- src/Chip8.c
+++ src/Chip8.c
@@ -1,5 +1,14 @@
 #include "Chip8.h"
 
+typedef void (*Chip8_Opcode)(Chip8 *c);
+
+Chip8_Opcode chip8_table[16] = 
+    {
+        &Chip8_Decode_0xxx, &Chip8_Decode_1xxx, &Chip8_Decode_2xxx, &Chip8_Decode_3xxx,
+        &Chip8_Decode_4xxx, &Chip8_Decode_5xxx, &Chip8_Decode_6xxx, &Chip8_Decode_7xxx,
+        &Chip8_Decode_8xxx, &Chip8_Decode_9xxx, &Chip8_Decode_Axxx, &Chip8_Decode_Bxxx,
+        &Chip8_Decode_Cxxx, &Chip8_Decode_Dxxx, &Chip8_Decode_Exxx, &Chip8_Decode_Fxxx
+    };
 
 unsigned char chip8_fontset[80] =
     {
@@ -43,4 +52,13 @@ void Chip8_Init(Chip8 *c){
             c->memory[i] = chip8_fontset[i];
         }
     }
+    srand(time(NULL));
+}
+
+void Chip8_EmulateCycle(Chip8 *c){
+    // Fetch
+    c->opcode = (c->memory[c->pc] << 8) | c->memory[c->pc + 1];
+    
+    // Decode and execute
+    chip8_table[(c->opcode & 0xF000) >> 12](c);
 }
